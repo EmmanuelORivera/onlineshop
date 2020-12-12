dca1eaa254333b98dbd056026f4100dedcfbd304
Emmanuel Ochoa <eorivera95@gmail.com>
2020-12-11 20:49:08 -0700

    Server controller and routes changes

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,14 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 dotenv.config();
 
 connectDB();
 
 const app = express();
+
+app.use(express.json());
 
 app.use(cors());
 
@@ -18,6 +21,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
