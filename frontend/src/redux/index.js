ee013a6f5aaf63c91bfd3f3a08e286f1829a2311
Emmanuel Ochoa <eorivera95@gmail.com>
2021-01-12 14:38:43 -0700

    Update user profile

--- frontend/src/redux/index.js
+++ frontend/src/redux/index.js
@@ -1,3 +1,8 @@
 export * from './product/productActions';
 export { addToCart, removeFromCart } from './cart/cartActions';
-export { login, logout } from './user/userActions';
+export {
+  login,
+  logout,
+  updateUserProfile,
+  getUserDetails,
+} from './user/userActions';
